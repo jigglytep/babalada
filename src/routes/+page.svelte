10fd158f1c8996ca0465ad3d7f00794150e8f304
mboyea <contact@mboyea.com>
2023-07-20 17:51:16 -0500

    styles added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,20 +5,34 @@
 	<h1>Welcome to Babalada!</h1>
 	<p>The best free & open source stock simulator on the web.</p>
 </section>
+<hr>
 <section>
 	<p>Learning to invest takes time and money. It is challenging to assert the difference between an expert investor and a charlatan.</p>
-	<p>But what if we could practice investing strategies without risk? What if we could prove the repeatability of investment strategies?</p>
-</section>
-<section>
+	<p>But what if we could practice investing strategies without risk? What if we could prove the repeatability of investment strategies? In Babalada, we can!</p>
 	<ul>
 		<li>Open multiple stock portfolios to practice investing strategies for free!</li>
 		<li>Discuss the best investment strategies in our forums!</li>
 		<li>Race to the top of the leaderboards, proving your skills to other users!</li>
 	</ul>
 </section>
+<hr>
 <section>
 	<p>Established in the community? Consider selling access to your private portfolios so other users can study your investment strategies.</p>
 </section>
 
-<style type="scss">
+<style lang="scss">
+	section {
+		padding: 0 10vw 0 10vw;
+		p	{
+			margin-top: 1rem;
+			margin-bottom: 1rem;
+		}
+	}
+	section:nth-of-type(1) {
+		height: 30%;
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
 </style>
